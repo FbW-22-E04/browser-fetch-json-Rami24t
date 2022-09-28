166ef588f9ce58c34f7c28a57aa2f871ef26986e
RamS24 <AlSaadi.Rami24@gmail.com>
2022-09-28 18:56:16 +0200

    solution

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,24 @@
 // The following line makes sure your styles are included in the project. Don't remove this.
 import '../styles/main.scss';
+import recipesString from './data.js'; 
 // Import any additional modules you want to include below \/
 
-
-// \/ All of your javascript should go here \/
-
+const recipes = JSON.parse(recipesString);
+Object.entries(recipes).forEach(cardGroup => {
+    const sections = [...document.querySelectorAll('main section')];
+    cardGroup[1].map(card => {
+        sections.forEach(section => {
+            if (card.type === section.id) {
+                let cardHTML = `<div class="card m-3" style="width: 18rem;">
+               <img src="${card.image}" class="card-img-top" alt="Baked Goods" height="200rem" />
+               <div class="card-body">
+                 <h4 class="card-title">${card.title}</h4>
+                 <h6>${card.author}</h6>
+                 <p class="border-top border-secondary pt-2">Ingredients: ${card.ingredients.join(", ")}</p>
+               </div>
+             </div>`
+                section.innerHTML += cardHTML;
+            }
+        });
+    });
+});
